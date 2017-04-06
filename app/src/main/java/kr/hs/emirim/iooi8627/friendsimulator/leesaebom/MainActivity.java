9167b5d95b51b0c1e36e2441064bdb160f6ee1a0
Kyungmin Lee <iooi8627@e-mirim.hs.kr>
2017-04-06 20:54:17 +0900

    #10 로그 남기기

--- app/src/main/java/kr/hs/emirim/iooi8627/friendsimulator/leesaebom/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/iooi8627/friendsimulator/leesaebom/MainActivity.java
@@ -1,36 +1,48 @@
 package kr.hs.emirim.iooi8627.friendsimulator.leesaebom;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    public final static String TAG = "이새봄: Main";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d(TAG, "onCreate 메소드 호출");
         setContentView(R.layout.activity_main);
+        Log.d(TAG, "activity_main 레이아웃 세팅");
     }
 
     void onClick (View view) {
+        Log.d(TAG, "onClick 메소드 호출");
+        Log.d(TAG, "클릭된 뷰 id:" + view.getId());
         switch (view.getId()) {
             case R.id.button1:
+                Log.d(TAG, "버튼1 클릭");
                 Toast.makeText(this, "너무 맛있어!", Toast.LENGTH_SHORT).show();
                 break;
             case R.id.button2:
+                Log.d(TAG, "버튼2 클릭");
                 Toast.makeText(this, "자퇴할 거야", Toast.LENGTH_SHORT).show();
                 break;
             case R.id.button3:
+                Log.d(TAG, "버튼3 클릭");
                 Toast.makeText(this, "집 언제 가지", Toast.LENGTH_SHORT).show();
                 break;
             case R.id.button4:
+                Log.d(TAG, "버튼4 클릭");
                 Toast.makeText(this, "너무 좋아!", Toast.LENGTH_SHORT).show();
                 break;
             case R.id.button5:
+                Log.d(TAG, "버튼5 클릭");
                 Toast.makeText(this, "침착해!", Toast.LENGTH_SHORT).show();
                 break;
             default:
+                Log.d(TAG, "? 클릭");
                 Toast.makeText(this, "다시 선택해 주세요", Toast.LENGTH_SHORT).show();
                 break;
         }
@@ -38,6 +50,7 @@ public class MainActivity extends AppCompatActivity {
         if (view.getId() == R.id.button1) {
             Toast.makeText(this, "작동해요", Toast.LENGTH_SHORT).show();
         }
+        Log.d(TAG, "onClick 메소드 무사히 종료");
     }
 
 }
